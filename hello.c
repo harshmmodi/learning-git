b115c508da07436c116ec45bab7d7054b1d08576
harshmmodi <harshmmodi@hotmail.com>
2018-05-16 13:10:49 +0530

    Added a comment

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main()
 {
 	printf("Angshu 0");	//First print from Angshu
-	printf("Harsh 0 Added on 16-01-2018");
+	printf("Harsh 0 Added on 16-01-2018"); // added by harsh
 	printf("Angshu 1");
 	printf("Angshu 2"); 
 
